1fffe7489363ad92a503b1081965b1592b7622ca
Filimonov Sergey <serifil1282@gmail.com>
2022-09-18 02:28:08 +0500

    initial

---
+++ src/main/java/ru/hogwarts/school/HogwartsSchool32Application.java
@@ -0,0 +1,12 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsSchool32Application {
+
+    public static void main (String[] args) {
+        SpringApplication.run(HogwartsSchool32Application.class, args);
+    }
+}
